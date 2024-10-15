a23e6f8b49d3bbc3477cc6a767863c8c88ef1b85
khuynh22 <timhuynhwork@gmail.com>
2024-10-15 12:06:22 -0700

    Update editing Name functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 export default function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(name);
+
+    function handleEdit() {
+        setIsEditing((editing) => !editing);
+    }
+
+    function handleSave(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let player = <span className="player-name">{playerName}</span>;
+    let buttonAction = "Edit";
+    if (isEditing) {
+        player = (
+            <input
+                type="text"
+                required value={playerName}
+                onChange={handleSave}
+            />
+        );
+        buttonAction = "Save";
+    }
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {player} 
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEdit}>{buttonAction}</button>
         </li>
     );
 }
